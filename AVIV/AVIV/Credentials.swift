34ec5509d2cdf986807eed657721807657ff8433
Lucas Hardman <lucas.hardman92@gmail.com>
2021-04-19 17:01:24 -0300

    Update Credentials.swift

--- AVIV/AVIV/Credentials.swift
+++ AVIV/AVIV/Credentials.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2019 Lucas Hardman. All rights reserved.
 //
 
+//Todas as credenciais e informações confidenciais (neste arquivo e em todos os outros) foram alteradas (e não são mais válidas) antes
+//desse repositório se tornar público
+
 public struct WatsonCredentials {
-    public static let personalityInsightsUsername = "" //Apagado para tornar público
-    public static let personalityInsightsPassword = "" //Apagado para tornar público
-    public static let languageTranslatorAPIKey = "" //Apagado para tornar público
+    public static let personalityInsightsUsername = "8cfff043-e219-4a89-834f-dcf7cf995587" 
+    public static let personalityInsightsPassword = "V1u3lTX48fsy" 
+    public static let languageTranslatorAPIKey = "woeFFgYbkbQE5kvSeeZ1H8JDdWEnOdyeG0BygfFQyA1C" 
     public static let languageTranslatorURL = "https://gateway.watsonplatform.net/language-translator/api"
     public static let version = "2019-03-01"
 }
